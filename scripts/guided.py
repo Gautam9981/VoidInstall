8dcb4b0a91a62cf7c0b5ba9e9e31ad954e927116
Gautam Mahajan <159855180+Gautam9981@users.noreply.github.com>
2025-08-25 22:09:15 -0400

    Refactor guided installation script to use launch_tui

--- scripts/guided.py
+++ scripts/guided.py
@@ -1,11 +1,10 @@
 """
 Guided installation script for Void Linux (voidinstall)
 """
-from tui.main import VoidInstallTUI
+from tui.main import launch_tui
 
 def run():
-    app = VoidInstallTUI()
-    app.run()
+    launch_tui()
 
 if __name__ == "__main__":
     run()
